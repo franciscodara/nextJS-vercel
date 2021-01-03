84905c89e5132efc7965536b99f5bf064f724a4b
Francisco Dara <franciscodara@gmail.com>
2021-01-03 15:14:45 -0300

    Home Back para teste

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
-import {useState} from 'react';
+// import {useState} from 'react';
 
 function Home() {
     return <div>
         <h1>Home</h1>
-        <Contador />
+        {/* <Contador /> */}
     </div>
 }
 
-function Contador () {
-    const [contador, setContador] = useState(1);
+// function Contador () {
+//     const [contador, setContador] = useState(1);
 
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
+//     function adicionarContador() {
+//         setContador(contador + 1);
+//     }
 
-    return (
-    <div>
-        <div>{contador}</div>
-        <button onClick={adicionarContador}>Adicionar</button>
-    </div>)
-}
+//     return (
+//     <div>
+//         <div>{contador}</div>
+//         <button onClick={adicionarContador}>Adicionar</button>
+//     </div>)
+// }
 
 export default Home
